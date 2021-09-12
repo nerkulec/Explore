cf18f23b94e000b065d446d99c4e65ed0b546537
nerkulec <nerkulec@github.com>
2021-09-12 11:51:46 +0200

    gaussian_demo

--- src/components/interacts/Gaussian.tsx
+++ src/components/interacts/Gaussian.tsx
@@ -1,83 +1,41 @@
-import React from "react"
+import React, { useMemo } from "react"
 import { useState } from "react"
 import Control from "../control/Control"
 import { useD3 } from "../RightSidebar"
 import {
   scaleLinear,
-  line,
-  range
+  range,
+  scaleSequentialLog,
+  extent,
+  contours,
+  geoPath,
+  interpolatePlasma,
+  scaleSequential
 } from 'd3'
 
 import "./Gaussian.css"
 import "../App.css"
 import { randn } from "../../evo/Evolution"
-import { v2 } from "../../evo/Agent"
 import { useEffect } from "react"
-import MathJax from "react-mathjax"
 
-const GaussianPlot = ({matrix, randoms}: {matrix: number[], randoms: [number, number][]}) => {
-  const ref = useD3((svg: any) => {
-    const width = +svg.attr('width')
-    const height = +svg.attr('height')
-    
-    const margin = { top: 20, right: 20, bottom: 20, left: 20 }
-    const innerWidth = width - margin.left - margin.right
-    const innerHeight = height - margin.top - margin.bottom
+type funType = {
+  value: (x: number, y: number) => number
+  optimum: [number, number]
+  scale: 'log' | 'lin'
+  thresholdRange: [number, number]
+  xDomain: number
+}
 
-    const xScale = scaleLinear()
-      .domain([-16, 16])
-      .range([0, innerWidth])
-    
-    const yScale = scaleLinear()
-      .domain([-12, 12])
-      .range([innerHeight, 0])
-    
-    const g = svg.select('g')
-      .attr('transform', `translate(${margin.left}, ${margin.top})`)
-
-    const areaGenerator = line()
-      .x(([x, y]) => xScale(x as any))
-      .y(([x, y]) => yScale(y as any))
-
-    const m = 100
-    const radiai = [10, 8, 6, 4, 2]
-    for (let i = 0; i < 5; i++) {
-      const radius = radiai[i]
-      const area = areaGenerator(range(m).map(i => {
-        const angle = 2*Math.PI*i/m
-        return [radius*Math.cos(angle), radius*Math.sin(angle)]
-      }))
-      g.select(`#area-${i}`)
-        .attr('d', area)
-    }
-    const data = randoms.map(([x, y]) => {
-      return [matrix[0]*x+matrix[2]*y, matrix[1]*x+matrix[3]*y]
-    })
-    g.selectAll("circle")
-      .remove()
-
-    g.selectAll("circle")
-      .data(data)
-      // .enter()
-      .join("circle")
-        .attr("cx", ([x, y]: v2) => xScale(x))
-        .attr("cy", ([x, y]: v2) => yScale(y))
-        .attr("r", 1.5)
-        .style("fill", "#ff0000")
-
-  }, [matrix, randoms])
-
-  return (
-    <svg ref={ref as any} width={480} height={360}>
-      <g>
-        <path id='area-0' className='area-0'/>
-        <path id='area-1' className='area-1'/>
-        <path id='area-2' className='area-2'/>
-        <path id='area-3' className='area-3'/>
-        <path id='area-4' className='area-4'/>
-      </g>
-    </svg>
-  )
+const functions: {
+  [key: string]: funType
+} = {
+  Rosenbrock: {
+    value: (x, y) => (1-x)**2+100*(y-x**2)**2,
+    optimum: [1, 1],
+    scale: 'log',
+    thresholdRange: [-1, 20],
+    xDomain: 8
+  }
 }
 
 
@@ -85,10 +43,19 @@ type Props = {
   
 }
 
-// !!! A FULL
 const Gaussian: React.FC<Props> = () => {
-  const [matrix, setMatrix] = useState(() => [1, 0, 0, 1])
-  const [randoms, setRandoms] = useState<[number, number][]>(() => [])
+  const [fun, setFun] = useState('Rosenbrock')
+  const [matrix, setMatrix] = useState(() => [1, 0, 1])
+  const [points, setPoints] = useState<[number, number][]>([[0, 0]])
+  const sqrt = useMemo(() => {
+
+
+
+    const s = Math.sqrt(matrix[0]*matrix[2]-matrix[1]*matrix[1])
+    const t = Math.sqrt(matrix[0]+matrix[2]+2*s)
+    const sqrt = [matrix[0]+s, matrix[1], matrix[1], matrix[2]+s].map(x => x/t)
+    return sqrt
+  }, [matrix])
 
   const setNth = (index: number) => (value: number) => setMatrix(matrix => {
     const newMatrix = [...matrix]
@@ -97,33 +64,176 @@ const Gaussian: React.FC<Props> = () => {
   })
 
   useEffect(() => {
-    setInterval(() => {
-      const randoms: [number, number][] = []
-      for (let i=0; i<100; i++) {
-        randoms.push([randn(), randn()])
-      }
-      setRandoms(randoms)
-    }, 100)
-  }, [setRandoms])
+    const interval = setInterval(() => {
+      setPoints(points => {
+        let [center, ...rest] = points
+        const {value} = functions[fun]
+        if (rest.length > 0) {
+          const [newCenter, ]: [[number, number], number] = rest.reduce(([best, bestValue], [x, y]) => {
+            const v = value(x, y)
+            if (value(x, y) < bestValue) {
+              return [[x, y], v]
+            } else {
+              return [best, bestValue]
+            }
+          }, [rest[0], value(...rest[0])])
+          center = newCenter
+        }
+        const newPoints: [number, number][] = [center]
+        for (let i=0; i<100; i++) {
+          const dx = randn()
+          const dy = randn()
+          newPoints.push([
+            center[0]+sqrt[0]*dx+sqrt[2]*dy,
+            center[1]+sqrt[1]*dx+sqrt[3]*dy
+          ])
+        }
+        return newPoints
+      })
+    }, 200)
+    return () => clearInterval(interval)
+  }, [points, setPoints, fun, sqrt])
+
+  const maxCorr = Math.floor(Math.sqrt(matrix[0]*matrix[2])*100)/100
 
   return <div className="Gaussian">
-    <MathJax.Provider>
-      <MathJax.Node formula={`\\begin{bmatrix}
-      ${matrix[0].toFixed(1)} & ${matrix[1].toFixed(1)} \\\\ ${matrix[2].toFixed(1)} & ${matrix[3].toFixed(1)}
-      \\end{bmatrix}`}/>
-    </MathJax.Provider>
-    <div className='row'>
-      <Control min={-10} max={10} step={0.1} label="" value={matrix[0]} setValue={setNth(0)}/>
-      <Control min={-10} max={10} step={0.1} label="" value={matrix[1]} setValue={setNth(1)}/>
-    </div>
-    <div className='row'>
-      <Control min={-10} max={10} step={0.1} label="" value={matrix[2]} setValue={setNth(2)}/>
-      <Control min={-10} max={10} step={0.1} label="" value={matrix[3]} setValue={setNth(3)}/>
+    <h2>Fitting the mutation distribution demo</h2>
+    <div>
+      <label>Function: </label>
+      <select value={fun} onChange={e => setFun(e.target.value)}>
+        <option>Rosenbrock</option>
+        <option>Grid with many local optima</option>
+        <option>Mountain car</option>
+      </select>
+      <Control min={0.05} max={3} step={0.01} label="" value={matrix[0]} setValue={setNth(0)}/>
+      <Control min={-maxCorr} max={maxCorr} step={0.01} label="" value={matrix[1]} setValue={setNth(1)}/>
+      <Control min={0.05} max={3} step={0.01} label="" value={matrix[2]} setValue={setNth(2)}/>
     </div>
     <div className='svg'>
-      <GaussianPlot matrix={matrix} randoms={randoms}/>
+      <GaussianPlot funInfo={functions[fun]} sqrt={sqrt} points={points}/>
     </div>
   </div>
+}
+
+const GaussianPlot = ({sqrt, points, funInfo}: {
+  sqrt: number[], points: [number, number][], funInfo: funType}) => {
+  const width = 960
+  const height = 720
+    
+  const margin = { top: 20, right: 20, bottom: 20, left: 20 }
+  const innerWidth = width - margin.left - margin.right
+  const innerHeight = height - margin.top - margin.bottom
+
+  const { value, optimum: [xOpt, yOpt], scale, xDomain, thresholdRange } = funInfo
+
+  const [center, ...randoms] = points
+
+  const xScale = scaleLinear()
+    .domain([xOpt-xDomain/2, xOpt+xDomain/2])
+    .range([0, innerWidth])
+
+  const yDomain = xDomain*0.75
+  const yScale = scaleLinear()
+    .domain([yOpt-yDomain/2, yOpt+yDomain/2])
+    .range([innerHeight, 0])
+  let thresholds
+  let scaleSeq
+  if (scale === 'log') {
+    thresholds = range(...thresholdRange).map(i => Math.pow(2, i))
+    scaleSeq = scaleSequentialLog
+  } else {
+    thresholds = range(...thresholdRange)
+    scaleSeq = scaleSequential
+  }
+  const color = scaleSeq(extent(thresholds) as any, interpolatePlasma)
+  const grid = useMemo(() => {
+    const q = 4
+    const x0 = -q / 2, x1 = width + 28 + q
+    const y0 = -q / 2, y1 = height + q
+    const n = Math.ceil((x1 - x0) / q)
+    const m = Math.ceil((y1 - y0) / q)
+    const grid = new Array(n * m) as any
+    for (let j = 0; j < m; ++j) {
+      for (let i = 0; i < n; ++i) {
+        grid[j * n + i] = value(xScale.invert(i * q + x0), yScale.invert(j * q + y0))
+      }
+    }
+    grid.x = -q
+    grid.y = -q
+    grid.k = q
+    grid.n = n
+    grid.m = m
+    return grid
+  }, [value, xScale, yScale])
+
+  const transform = ({type, value, coordinates}:
+    {type: string, value: (x: number, y: number) => number, coordinates: [number, number][][][]}) => ({
+      type, value, coordinates: coordinates.map(rings => 
+      rings.map(points => 
+        points.map(([x, y]) => ([
+          grid.x + grid.k * x,
+          grid.y + grid.k * y
+        ]))
+      )
+    )
+  })
+  
+  const fcontours = contours()
+    .size([grid.n, grid.m])
+    .thresholds(thresholds)(grid)
+    .map(transform as any)
+
+  const circles = randoms.map(([x, y]) => ({
+    x: xScale(x), y: yScale(y), r: 1.5, fill: '#000'
+  }))
+
+  const ref = useD3((svg: any) => {
+    svg.select('.contour')
+      .attr('fill', 'none')
+      .attr('stroke', '#fff')
+      .attr('stroke-opacity', 0.2)
+      .selectAll('path')
+      .data(fcontours)
+      .join('path')
+        .attr('fill', (d: any) => color(d.value))
+        .attr('d', geoPath())
+
+    svg.select('#center')
+      .attr('cx', xScale(center[0]))
+      .attr('cy', yScale(center[1]))
+      .attr('r', 3)
+      .attr('fill', '#00FF00')
+
+    svg.select('#optimum')
+      .attr('cx', xScale(xOpt))
+      .attr('cy', yScale(yOpt))
+      .attr('r', 3)
+      .attr('fill', '#FF0000')
+
+  }, [sqrt, points])
+
+  return (
+    <svg ref={ref as any} width={960} height={720}>
+      <g id='main' transform={`translate(${margin.left}, ${margin.top})`}>
+        <rect id='bg' width={innerWidth} height={innerHeight} fill='#000'/>
+        <g className='contour' />
+        <g className='dots'>
+          <circle id='center'/>
+          <circle id='optimum'/>
+        </g>
+        <g id='elipses'>
+          <path id='area-0' className='ellipse area-0'/>
+          <path id='area-1' className='ellipse area-1'/>
+          <path id='area-2' className='ellipse area-2'/>
+          <path id='area-3' className='ellipse area-3'/>
+          <path id='area-4' className='ellipse area-4'/>
+          {
+            circles.map(({x, y, fill, r}) => <circle cx={x} cy={y} r={r} fill={fill}/>)
+          }
+        </g>
+      </g>
+    </svg>
+  )
 }
 
 export default Gaussian
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App'
+import Gaussian from './components/interacts/Gaussian'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom"
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Switch>
+        <Route path='/correlation-matrix'>
+          <Gaussian />
+        </Route>
+        <Route path='/'>
+          <App />
+        </Route>
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 )
